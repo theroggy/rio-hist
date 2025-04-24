21841c10bc4034c56e5451e6300cbd4a9d9fea9b
Pieter Roggemans <pieter.roggemans@gmail.com>
2025-04-24 14:52:37 +0200

    MAINT: use ruff and pre-commit to lint and format code

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 # Parse the version from the fiona module.
-with open('rio_hist/__init__.py') as f:
+with open("rio_hist/__init__.py") as f:
     for line in f:
         if line.find("__version__") >= 0:
             version = line.split("=")[1].strip()
@@ -19,25 +19,27 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-setup(name='rio-hist',
-      version=version,
-      description=u"Histogram matching plugin for rasterio",
-      long_description=long_description,
-      classifiers=[],
-      keywords='',
-      author=u"Matthew Perry",
-      author_email='perry@mapbox.com',
-      url='https://github.com/mapbox/rio-hist',
-      license='BSD',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=["click", "rasterio~=1.0", "rio_color>=0.4"],
-      extras_require={
-          'plot': ['matplotlib'],
-          'test': ['pytest', 'pytest-cov', 'codecov']},
-      entry_points="""
+setup(
+    name="rio-hist",
+    version=version,
+    description="Histogram matching plugin for rasterio",
+    long_description=long_description,
+    classifiers=[],
+    keywords="",
+    author="Matthew Perry",
+    author_email="perry@mapbox.com",
+    url="https://github.com/mapbox/rio-hist",
+    license="BSD",
+    packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=["click", "rasterio~=1.0", "rio_color>=0.4"],
+    extras_require={
+        "plot": ["matplotlib"],
+        "test": ["pytest", "pytest-cov", "codecov"],
+    },
+    entry_points="""
       [rasterio.rio_plugins]
       hist=rio_hist.scripts.cli:hist
-      """
-      )
+      """,
+)
